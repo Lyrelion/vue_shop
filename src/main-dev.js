5ce3b7493cf059bead72f7030178437ecacb01f8
tea <zheyuan54@qq.com>
2020-08-23 08:04:20 +0800

    feat: 优化发布及打包入口文件，进行CDN缓存

--- src/main-dev.js
+++ src/main-dev.js
@@ -8,58 +8,57 @@ import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
-
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-// require styles 导入富文本编辑器对应的样式
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
-
-// 导入 NProgress 包对应的JS和CSS
+// 导入 NProgress
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
 import axios from 'axios'
-// 配置请求的跟路径
+// 配置请求的根路径
+// 如果是发布环境，这里的路径就要改为服务器线上部署的地址
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-    // 在 request 拦截器中，展示进度条 NProgress.start()
+// 请求拦截器
 axios.interceptors.request.use(config => {
-        // console.log(config)
-        NProgress.start()
-        config.headers.Authorization = window.sessionStorage.getItem('token')
-            // 在最后必须 return config
-        return config
-    })
-    // 在 response 拦截器中，隐藏进度条 NProgress.done()
-axios.interceptors.response.use(config => {
-    NProgress.done()
-    return config
+  // console.log(config)
+  // 添加进度条效果
+  NProgress.start()
+  // 每次请求发送之前，都要在请求头中加入token，从本地存储中获取
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
 })
+// 响应拦截器
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+// 将 axios 挂载到vue原型上，这样全局都能通过 this.$http.xxx 引用
 Vue.prototype.$http = axios
-
+// 是否在控制台中提示当前位于开发/生产环境
 Vue.config.productionTip = false
 
-// 将树形组件注册为全局可用的组件
+// 将树形组件，注册为全局可用的组件
 Vue.component('tree-table', TreeTable)
-    // 将富文本编辑器，注册为全局可用的组件
+// 将富文本编辑器，注册为全局可用的组件
 Vue.use(VueQuillEditor)
+// 全局过滤器，时间格式化处理
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  // padStart(2, '0')：字符串补充方法，总长度为几位，如果长度不足，用什么字符补充
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
 
-Vue.filter('dateFormat', function(originVal) {
-    const dt = new Date(originVal)
-
-    const y = dt.getFullYear()
-    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
-    const d = (dt.getDate() + '').padStart(2, '0')
-
-    const hh = (dt.getHours() + '').padStart(2, '0')
-    const mm = (dt.getMinutes() + '').padStart(2, '0')
-    const ss = (dt.getSeconds() + '').padStart(2, '0')
-
-    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
 })
 
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  render: h => h(App)
 }).$mount('#app')
--- src/main-prod.js
+++ src/main-prod.js
@@ -7,54 +7,44 @@ import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
-
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-
-// 导入 NProgress 包对应的JS和CSS
+// 导入 NProgress
 import NProgress from 'nprogress'
 
 import axios from 'axios'
-// 配置请求的跟路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-    // 在 request 拦截器中，展示进度条 NProgress.start()
+// 配置请求的根路径
+// 如果是发布环境，这里的路径就要改为服务器线上部署的地址
+axios.defaults.baseURL = 'http://m2.sufaith.com/api/private/v1/'
 axios.interceptors.request.use(config => {
-        // console.log(config)
-        NProgress.start()
-        config.headers.Authorization = window.sessionStorage.getItem('token')
-            // 在最后必须 return config
-        return config
-    })
-    // 在 response 拦截器中，隐藏进度条 NProgress.done()
-axios.interceptors.response.use(config => {
-    NProgress.done()
-    return config
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
 })
-Vue.prototype.$http = axios
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
-    // 将富文本编辑器，注册为全局可用的组件
 Vue.use(VueQuillEditor)
-    // 定义全局过滤器 每个组件都可以调用
 Vue.filter('dateFormat', function(originVal) {
-    const dt = new Date(originVal) // 接收后台传来的时间
+  const dt = new Date(originVal)
+  // padStart(2, '0')：字符串补充方法，总长度为几位，如果长度不足，用什么字符补充
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
 
-    const y = dt.getFullYear()
-        // padStart(2, '0')：字符串补充方法，总长度为几位，如果长度不足，用什么字符补充
-    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
-    const d = (dt.getDate() + '').padStart(2, '0')
-
-    const hh = (dt.getHours() + '').padStart(2, '0')
-    const mm = (dt.getMinutes() + '').padStart(2, '0')
-    const ss = (dt.getSeconds() + '').padStart(2, '0')
-
-    // 模板字符串
-    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
 })
 
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  render: h => h(App)
 }).$mount('#app')
